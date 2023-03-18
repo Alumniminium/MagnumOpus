6c72fb8de2593e36fba677113c4fd894e40e4446
trbl <domi@outlook.at>
2023-03-18 20:16:14 +0100

    reset

--- Networking/Co2Packet.cs
+++ Networking/Co2Packet.cs
@@ -12,25 +12,6 @@ namespace MagnumOpus.Networking
 {
     public static unsafe class Co2Packet
     {
-<<<<<<< HEAD
-        public static void Serialize<T>(ref NetworkComponent net, ref T packetStruct) where T : unmanaged
-        {
-            lock(net.Username)
-            {
-                var size = typeof(T) == typeof(MsgDHX) ? 355 : sizeof(T);
-                var requiredSize = net.SendBufferOffset + size;
-
-                if (requiredSize > net.SendBuffer.Length)
-                    Debugger.Break();
-
-                var buffer = net.SendBuffer.Span.Slice(net.SendBufferOffset, size);
-                MemoryMarshal.Write(buffer, ref packetStruct);
-
-                // Increment SendBufferOffset
-                var actualSize = typeof(T) == typeof(MsgDHX) ? 355 : BitConverter.ToUInt16(buffer[..2]);
-                net.SendBufferOffset += actualSize;
-            }
-=======
         public static Memory<byte> Serialize<T>(ref T pacetStruct) where T : unmanaged
         {
             var buffer = new byte[typeof(T) == typeof(MsgDHX) ? 355 : sizeof(T)];
@@ -38,7 +19,6 @@ namespace MagnumOpus.Networking
             var size = typeof(T) == typeof(MsgDHX) ? 355 : BitConverter.ToUInt16(buffer, 0);
 
             return buffer.AsMemory()[0..size];
->>>>>>> 3161883ebe68e1efedc7baa80d392375ebd53323
         }
 
         public static T Deserialize<T>(Span<byte> buffer) where T : unmanaged
